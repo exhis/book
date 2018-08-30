edceb24f3466525585c0f32f91d1d2a4e50552ce
yang.liang <yang.liang@moker.com>
2018-08-30 14:35:01 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png" @click="aaa()"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {},
+  methods: {}
+};
+</script>
+
+<style>
+#app{
+  width: 100%;
+  height: 100%;
+}
+</style>
